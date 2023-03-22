f6d32ad8b355642d8cd9c3d024a2b118ae615ad5
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-22 09:48:25 +0200

    update

--- java/Main.java
+++ java/Main.java
@@ -3,13 +3,19 @@ import classes.SpaceShip;
 public class Main {
     public static void main(String[] args) {
         SpaceShip ship = new SpaceShip();
-        ship.setName("Walker");
-        System.out.println(ship.getName()); //Should be Walker
+//        ship.setName("Walker");
+//        System.out.println(ship.getName()); //Should be Walker
+//
+//        ship.setName("");
+//        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+//
+//        ship.setName("Voyager ".repeat(100));
+//        System.out.println(ship.getName()); //Should be Walker, too long value ignored
 
-        ship.setName("");
-        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+        ship.setSerialNumber("SN506788");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788
 
-        ship.setName("Voyager ".repeat(100));
-        System.out.println(ship.getName()); //Should be Walker, too long value ignored
+        ship.setSerialNumber("EE123456");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
     }
 }
