6163796e98c43ba1d34af0dc92d54564fc884630
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-25 15:10:50 +0200

    final class Engine, Tanker

---
+++ java/classes/MX200Tanker.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class MX200Tanker extends Tanker {
+    @Override
+    protected String getFuelType() {
+        return "MX200";
+    }
+}
---
+++ java/classes/XFuelTanker.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class XFuelTanker extends Tanker {
+    @Override
+    protected String getFuelType() {
+        return "XFuel";
+    }
+}
