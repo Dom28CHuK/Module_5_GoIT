35df344ec829cba6e5346467083ea9c9e871c07e
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-25 08:35:10 +0200

    add class SpaceRocketLauncher

--- java/Main.java
+++ java/Main.java
@@ -1,3 +1,4 @@
+import classes.SpaceRocketLauncher;
 import classes.SpaceShip;
 
 public class Main {
@@ -18,10 +19,24 @@ public class Main {
 //        ship.setSerialNumber("EE123456");
 //        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
 
-        ship.setName("Voyager");
-        ship.setSerialNumber("SN506788");
+//        ship.setName("Voyager");
+//        ship.setSerialNumber("SN506788");
+//
+//        //Name is Voyager, serial number is SN504030
+//        ship.printInfo();
 
-        //Name is Voyager, serial number is SN504030
-        ship.printInfo();
+        SpaceRocketLauncher launcher = new SpaceRocketLauncher();
+        launcher.setBigRocketCount(5);
+        launcher.setSmallRocketCount(10);
+
+        System.out.println("Power is " + launcher.getTotalPower());
+
+        launcher.launchBigRocket();
+        System.out.println("Big rocket count is " + launcher.getBigRocketCount());
+
+        laucnher.launchSmallRocket();
+        System.out.println("Small rocket count is " + laucnher.getSmallRocketCount());
+
+        System.out.println("Power is " + launcher.getTotalPower());
     }
 }
---
+++ java/classes/SpaceRocketLauncher.java
@@ -0,0 +1,32 @@
+package classes;
+
+public class SpaceRocketLauncher {
+    private int bigRocketCount;
+    private int smallRocketCount;
+
+    public int getBigRocketCount() {
+        return bigRocketCount;
+    }
+
+    public void setBigRocketCount(int bigRocketCount) {
+        this.bigRocketCount = bigRocketCount;
+    }
+
+    public int getSmallRocketCount() {
+        return smallRocketCount;
+    }
+
+    public void setSmallRocketCount(int smallRocketCount) {
+        this.smallRocketCount = smallRocketCount;
+    }
+
+    public void launchBigRocket(int bigRocketCount) {
+
+    }
+    public void launchSmallRocket(int smallRocketCount) {
+
+    }
+    public int getTotalPower() {
+
+    }
+}
