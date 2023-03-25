5d34e9d44a6e334e97d33bf4b7f8c004065cbc1b
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-25 14:51:40 +0200

    add new class AdvancedXFuelEngine

--- java/classes/XFuelEngine.java
+++ java/classes/XFuelEngine.java
@@ -1,6 +1,13 @@
 package classes;
 
 public class XFuelEngine extends Engine {
+
+    protected String serialNumber;
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
     @Override
     public String getFuelType() {
         return "XFuel";
