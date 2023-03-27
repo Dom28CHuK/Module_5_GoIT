757782f21ca54b0b7b5bd71d61c3d17e2382b138
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-27 18:14:18 +0300

    add class SpaceUtils

--- java/Main.java
+++ java/Main.java
@@ -1,10 +1,16 @@
 import classes.Planets;
+import classes.SpaceUtils;
 import classes.SpaceportMessages;
 
 public class Main {
     public static void main(String[] args) {
 //        System.out.println(Planets.MERCURY);
 
-        SpaceportMessages.generateLandingMessage("SunFlower", "EarthKeeper", "20:40");
+//        SpaceportMessages.generateLandingMessage("SunFlower", "EarthKeeper", "20:40");
+
+        System.out.println(SpaceUtils.PLANET_COUNT);//8
+        System.out.println(SpaceUtils.pluralPlanets(1));//1 planet
+        System.out.println(SpaceUtils.pluralPlanets(5));//5 planets
+        System.out.println(SpaceUtils.generateStartMessage("Earthkeeper", "20:40"));
     }
 }
