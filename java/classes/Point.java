953d580af8a23894bd93a21bb806c61f029b495a
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-27 18:38:29 +0300

    add class StarTrack

--- java/classes/Point.java
+++ java/classes/Point.java
@@ -38,4 +38,9 @@ public class Point {
         Point point = (Point) o;
         return x == point.x && y == point.y && z == point.z;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
 }
