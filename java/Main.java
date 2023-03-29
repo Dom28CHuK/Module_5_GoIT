25f3e93917f61daf974cfa7fca7ad75b04d9fb92
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-29 12:32:49 +0300

    add two abstract classes

--- java/Main.java
+++ java/Main.java
@@ -1,10 +1,11 @@
-import classes.*;
+import classes.Mars;
+import classes.Moon;
+import classes.PlanetTester;
 
 public class Main {
     public static void main(String[] args) {
-        Stars stars = new Stars();
-        stars.setCount(153);
-        System.out.println(stars);
-        System.out.println(stars.getCount());
+        PlanetTester tester = new PlanetTester();
+        System.out.println(tester.test(new Mars())); //Should be "planet"
+        System.out.println(tester.test(new Moon())); //Should be "not planet"
     }
 }
