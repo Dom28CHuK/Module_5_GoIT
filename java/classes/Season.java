a3bf6974016b1eaa46da9516dd7e8dfe28a21f5d
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-30 12:41:31 +0300

    add new class

--- java/classes/Season.java
+++
@@ -1,6 +0,0 @@
-package classes;
-
-public abstract class Season {
-    public abstract String getName();
-    public abstract boolean isCold();
-}
--- java/classes/SeasonTest.java
+++
@@ -1,7 +0,0 @@
-package classes;
-
-public class SeasonTest {
-    public String test(Season season) {
-        return season.getName() + (season.isCold() ? " is cold" : " is warm");
-    }
-}
