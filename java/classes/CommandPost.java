c2ab4f87a70e8e234d4c0c801fc93dbb243342ca
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-30 13:41:05 +0300

    update final

---
+++ java/classes/CommandPost.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class CommandPost {
+    public void command() {
+        System.out.println("Fire!");
+    }
+}
--- java/classes/Spaceport.java
+++ java/classes/Spaceport.java
@@ -4,11 +4,13 @@ public class Spaceport {
     private GasStation gasStation = new GasStation();
     private ServiceTower serviceTower = new ServiceTower();
     private CompressorStation compressorStation = new CompressorStation();
+    private CommandPost commandPost = new CommandPost();
     private Radar radar = new Radar();
     public void launch() {
         gasStation.refuel();
         serviceTower.service();
         compressorStation.compress();
         radar.scan();
+        commandPost.command();
     }
 }
