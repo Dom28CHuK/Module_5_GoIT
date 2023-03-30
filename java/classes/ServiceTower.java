797f20d88b52c5678c1d35cbd0be9d47bb940529
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-30 13:20:16 +0300

    update

---
+++ java/classes/ServiceTower.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class ServiceTower {
+    public void service() {
+        System.out.println("Service done!");
+    }
+}
--- java/classes/Spaceport.java
+++ java/classes/Spaceport.java
@@ -2,7 +2,9 @@ package classes;
 
 public class Spaceport {
     private GasStation gasStation = new GasStation();
+    private ServiceTower serviceTower = new ServiceTower();
     public void launch() {
         gasStation.refuel();
+        serviceTower.service();
     }
 }
